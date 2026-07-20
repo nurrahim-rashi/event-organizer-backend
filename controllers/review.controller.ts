4630856ddcb1733982f1b0e1aa1d30fbe1fd771b
rashifanurrahim <rashifa.nurrahim@gmail.com>
2026-07-20 03:57:51 -0400

    fix: erase console.log

--- controllers/review.controller.ts
+++ controllers/review.controller.ts
@@ -3,21 +3,13 @@ import { createReviewService } from "../services/review.service.js";
 import { createReviewSchema } from "../validators/review.validator.js";
 
 export const postReviewController = async (req: any, res: Response) => {
-  console.log("DEBUG: Controller dipanggil"); // 1
-
   const validatedData = createReviewSchema.parse(req.body);
-  console.log("DEBUG: Zod validasi lolos"); // 2
-
   const userId = req.user.id;
-  console.log("DEBUG: User ID:", userId); // 3
-
   const review = await createReviewService(
     userId,
     validatedData.transactionId,
     { rating: validatedData.rating, comment: validatedData.comment },
   );
-  console.log("DEBUG: Service selesai", review); // 4
-
   return res.status(201).json({
     message: "Review created successfully",
     data: review,
