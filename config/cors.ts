69ee31744c7aad5fe78e6c223f81f59ed488f274
rashifanurrahim <rashifa.nurrahim@gmail.com>
2026-07-26 04:43:57 -0400

    fix: cors

--- config/cors.ts
+++ config/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 export const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://event-organizer-omega.vercel.app"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
